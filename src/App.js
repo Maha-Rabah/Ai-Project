f9e5c763a6255895d05853da43c6da686ba4001d
Maha-Rabah <maharabah13@gmail.com>
2023-11-14 02:10:37 +0300

    new commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Chat from '../src/modules/chat/pages/Chat.jsx'
+import Artificium from '../src/modules/artificium/pages/Artificium.jsx'
+function App() {
+  return (
+    <div>
+      <Artificium/>
+    </div>
+  );
+}
+
+export default App;
